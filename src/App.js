05f00e2a72580f9c7f978b28bc79ab1ff89a6893
Nick <nickmagi24@gmail.com>
2021-11-30 17:22:22 -0500

    Header component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from "react";
+import Header from "./components/Header";
+import './style.css';
+
 
 function App() {
   return (
-    <div className="App">
+    <div className='app'>
+      <Header />
+      {/* <Timer /> */}
+        {/* Input for timer */}
+        {/* Start button */}
 
     </div>
   );
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+	return (
+		<header>
+			<div className="title">Tranquility</div>
+		</header>
+	)
+}
+
+export default Header;
