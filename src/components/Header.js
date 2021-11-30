00cc3f7f2eabf29f177a9d76dbcc2a5c66dce1d1
Nick <nickmagi24@gmail.com>
2021-11-30 18:31:27 -0500

    Responsive header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,18 +3,20 @@ import Gear from '../imgs/gear.png'
 
 function Header() {
 	return (
-		<header>
-			<div className="left-side">
-				<h1 className="title">Tranquility</h1>
-				<div className="line-divider"></div>
-			</div>
+		<>
+			<header>
+				<div className="left-side">
+					<h1 className="title">Tranquility</h1>
+				</div>
 
-			<div className="right-side">
-				<button className="settings-button">
-					<img id="gear" src={Gear} />
-				</button>
-			</div>
-		</header>
+				<div className="right-side">
+					<button className="settings-button">
+						<img id="gear" src={Gear} />
+					</button>
+				</div>
+			</header>
+			<div className="line-divider"></div>
+		</>
 	)
 }
 
