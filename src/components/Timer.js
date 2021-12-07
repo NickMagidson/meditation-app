a8d003c87ea55ea577f9ed7ff083508980142dd0
Nick <nickmagi24@gmail.com>
2021-12-07 11:17:59 -0500

    Basic pomodoro logic

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,27 +1,48 @@
-import React, { Component } from "react";
+import React, { Component, useState, useEffect } from "react";
+
+
+
 
 function Timer() {
+  const [minutes, setMinutes] = useState(25);
+  const [seconds, setSeconds] = useState(0);
+  const [displayMessage, setDisplayMessage] = useState(false);
 
+  useEffect(() => {
+    let interval = setInterval(() => {
+      clearInterval(interval);
 
+      if (seconds === 0) {
+        if (minutes !== 0) {
+          setSeconds(59);
+          setMinutes(minutes - 1);
+        } else {
+          // display message / trigger sound
+        }
+      } else {
+        setSeconds(seconds - 1);
+      }
+    }, 1000)
+  }, [seconds])
+
+  const timerMinutes = minutes < 10 ? `0${minutes}`: minutes;
+  const timerSeconds = seconds < 10 ? `0${seconds}` : seconds;
 
   return (
     <>
-        <div className="grey-box">
-
-          <div className="number-box">
-            <h2 className="timer-text">10:00</h2>
-          </div>
-
-          <div className="buttons-container">
-            <button className="timer-button">
-              <img 
-                className="play" 
-                src="https://img.icons8.com/ios-glyphs/90/ffffff/play--v1.png" 
-              />
-            </button>
-          </div>
-
+      <div className="grey-box">
+        <div className="number-box">
+          <h2 className="timer-text">{timerMinutes}:{timerSeconds}</h2>
         </div>
+        <div className="buttons-container">
+          <button className="timer-button">
+            <img 
+              className="play" 
+              src="https://img.icons8.com/ios-glyphs/90/ffffff/play--v1.png" 
+            />
+          </button>
+        </div>
+      </div>
     </>
     );
   }
