f6d8f80dd5109f8989d5cb4dc309a5cfb3a78dd7
Nick <nickmagi24@gmail.com>
2021-12-07 12:42:03 -0500

    Handle click implementation

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -6,27 +6,28 @@ import React, { Component, useState, useEffect } from "react";
 function Timer() {
   const [minutes, setMinutes] = useState(25);
   const [seconds, setSeconds] = useState(0);
-  const [displayMessage, setDisplayMessage] = useState(false);
 
-  useEffect(() => {
+  function handleClick() {
     let interval = setInterval(() => {
       clearInterval(interval);
-
       if (seconds === 0) {
         if (minutes !== 0) {
           setSeconds(59);
           setMinutes(minutes - 1);
         } else {
-          // display message / trigger sound
+          // trigger sound
         }
       } else {
         setSeconds(seconds - 1);
       }
     }, 1000)
-  }, [seconds])
+  }
+
 
   const timerMinutes = minutes < 10 ? `0${minutes}`: minutes;
   const timerSeconds = seconds < 10 ? `0${seconds}` : seconds;
+
+
 
   return (
     <>
@@ -35,7 +36,7 @@ function Timer() {
           <h2 className="timer-text">{timerMinutes}:{timerSeconds}</h2>
         </div>
         <div className="buttons-container">
-          <button className="timer-button">
+          <button className="timer-button" onClick={handleClick}>
             <img 
               className="play" 
               src="https://img.icons8.com/ios-glyphs/90/ffffff/play--v1.png" 
