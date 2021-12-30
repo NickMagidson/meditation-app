96b9539ce7ff513278b334f0a272461b43cf3562
Nick <nickmagi24@gmail.com>
2021-12-29 22:52:09 -0500

    Modal component setup

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Gear from '../imgs/gear.png'
+import Modal from "./Modal";
 
 function Header() {
 	return (
@@ -13,6 +14,11 @@ function Header() {
 					<button className="settings-button">
 						<img id="gear" src={Gear} />
 					</button>
+
+					<Modal>
+						Settings Menu
+					</Modal>
+					 
 				</div>
 			</header>
 			<div className="line-divider"></div>
